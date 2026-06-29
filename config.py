a173e76b42eeec1c1afd28000f7ae2934e554051
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-06-29 20:12:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,16 @@
+import os
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
+
+# Telegram Bot
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Admin
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+# Required Channel
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")
