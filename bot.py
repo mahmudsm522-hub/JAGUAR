8da60cbf30c88aeee1b26b686a6f61954d7f2ab9
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-06-30 14:56:59 +0100

    Refactor bot.py to simplify initialization and commands

--- bot.py
+++ bot.py
@@ -1,51 +1,20 @@
-"""
-=====================================
-🐆 Jaguar Telegram Bot
-Version : v0.1
-=====================================
-"""
-
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Message
 
 from config import BOT_TOKEN
+from handlers.start import router
 
-
-# ==========================
-# Create Bot & Dispatcher
-# ==========================
-
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
+dp.include_router(router)
 
-# ==========================
-# Start Command
-# ==========================
-
-@dp.message(CommandStart())
-async def start(message: Message):
-    await message.answer(
-        "🐆 Welcome to Jaguar!\n\n"
-        "Jaguar is starting successfully."
-    )
-
-
-# ==========================
-# Main Function
-# ==========================
 
 async def main():
-    print("🐆 Jaguar Bot is Running...")
+    print("🐆 Jaguar Bot Started")
     await dp.start_polling(bot)
 
-
-# ==========================
-# Run Bot
-# ==========================
 
 if __name__ == "__main__":
     asyncio.run(main())
