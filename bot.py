67884761170cab02c9cf7ea960f88ea1816d5289
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-06-30 20:34:25 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,33 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
-from config import BOT_TOKEN
-from handlers.start import router
+from bot.config import BOT_TOKEN
 
-bot = Bot(BOT_TOKEN)
-dp = Dispatcher()
-
-dp.include_router(router)
+# Routers
+from handlers.start import router as start_router
+from handlers.menu import router as menu_router
 
 
 async def main():
-    print("🐆 Jaguar Bot Started")
+
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(
+            parse_mode=ParseMode.HTML
+        )
+    )
+
+    dp = Dispatcher()
+
+    # Register Routers
+    dp.include_router(start_router)
+    dp.include_router(menu_router)
+
+    print("🐆 Jaguar Bot Started Successfully!")
+
     await dp.start_polling(bot)
 
 
