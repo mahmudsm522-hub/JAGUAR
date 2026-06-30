8034ca5f73d6cb72c20778dc8baf654c4c5179db
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-06-30 20:07:42 +0100

    Create checker.py

---
+++ utils/checker.py
@@ -0,0 +1,51 @@
+from aiogram.exceptions import TelegramBadRequest
+from bot.config import CHANNEL_ID
+from utils.database import cursor
+
+
+async def is_joined(bot, user_id):
+    """
+    Check if user joined the main channel.
+    """
+    try:
+        member = await bot.get_chat_member(CHANNEL_ID, user_id)
+
+        if member.status in (
+            "member",
+            "administrator",
+            "creator"
+        ):
+            return True
+
+    except TelegramBadRequest:
+        pass
+
+    return False
+
+
+def is_banned(user_id):
+    """
+    Returns True if user is banned.
+    """
+
+    cursor.execute(
+        "SELECT banned FROM users WHERE user_id=?",
+        (user_id,)
+    )
+
+    result = cursor.fetchone()
+
+    if result:
+        return result[0] == 1
+
+    return False
+
+
+def is_admin(user_id):
+    """
+    Check if user is bot admin.
+    """
+
+    from bot.config import ADMINS
+
+    return user_id in ADMINS
