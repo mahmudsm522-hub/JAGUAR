3c163bebe6dc03fb7588e65989401b95c7be34b1
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-07-02 18:12:44 +0100

    Update wallet.py

--- handlers/user/wallet.py
+++ handlers/user/wallet.py
@@ -1,1 +1,32 @@
+from aiogram import Router, F
+from aiogram.types import Message
 
+from utils.database import get_balance
+
+router = Router()
+
+
+@router.message(F.text == "👛 Wallet")
+async def wallet_menu(message: Message):
+
+    user_id = message.from_user.id
+
+    balance = get_balance(user_id)
+
+    text = f"""
+👛 <b>JAGUAR WALLET</b>
+
+━━━━━━━━━━━━━━
+
+💰 Balance:
+<b>{balance} JGR</b>
+
+💸 Minimum Withdraw:
+<b>10,000 JGR</b>
+
+━━━━━━━━━━━━━━
+
+Choose an option below.
+"""
+
+    await message.answer(text)
