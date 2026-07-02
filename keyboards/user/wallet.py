326556d79472ab0121885b47c1634aa9c59017fe
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-07-02 18:23:50 +0100

    Update wallet.py

--- keyboards/user/wallet.py
+++ keyboards/user/wallet.py
@@ -1,1 +1,18 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+wallet_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="💸 Withdraw",
+                callback_data="withdraw"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="📜 Withdraw History",
+                callback_data="withdraw_history"
+            )
+        ]
+    ]
+)
