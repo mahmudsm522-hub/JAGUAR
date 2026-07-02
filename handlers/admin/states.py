0a3b68526443192d07580db1e16909fee4af1a62
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-07-02 15:21:51 +0100

    Create states.py

---
+++ handlers/admin/states.py
@@ -0,0 +1,8 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class AddTask(StatesGroup):
+    waiting_for_title = State()
+    waiting_for_type = State()
+    waiting_for_link = State()
+    waiting_for_reward = State()
