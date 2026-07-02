3973649e01ac4ad35d9f0fbb7bfd931414e82e1f
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-07-02 21:31:09 +0100

    Update states.py

--- handlers/admin/states.py
+++ handlers/admin/states.py
@@ -6,3 +6,5 @@ class AddTask(StatesGroup):
     waiting_for_type = State()
     waiting_for_link = State()
     waiting_for_reward = State()
+class BroadcastState(StatesGroup):
+    waiting_message = State()
