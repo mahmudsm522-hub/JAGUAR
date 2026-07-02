789f1bcbb27681cbf5294fc87e039e39de5c0c7b
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-07-02 20:44:49 +0100

    Add withdraw menu for wallet options

--- handlers/user/wallet.py
+++ handlers/user/wallet.py
@@ -2,7 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 from keyboards.user.wallet import withdraw_type_keyboard
 from utils.database import get_balance
-
+from aiogram.types importCallbackQuery
 router = Router()
 
 
@@ -30,3 +30,13 @@ Choose an option below.
 """
 
     await message.answer(text)
+
+@router.callback_query(F.data == "withdraw")
+async def withdraw_menu(callback: CallbackQuery):
+
+    await callback.message.answer(
+        "💸 Select Wallet Type",
+        reply_markup=withdraw_type_keyboard
+    )
+
+    await callback.answer()
