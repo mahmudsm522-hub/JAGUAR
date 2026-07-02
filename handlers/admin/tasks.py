36c24e689ebfcdaab72fd24bf545b872f40c65e0
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-07-02 15:11:35 +0100

    Update tasks.py

--- handlers/admin/tasks.py
+++ handlers/admin/tasks.py
@@ -1,1 +1,24 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
 
+from bot.config import ADMIN_ID
+from utils.database import add_task
+
+router = Router()
+
+
+@router.message(Command("addtask"))
+async def add_task_command(message: Message):
+
+    if message.from_user.id != ADMIN_ID:
+        return
+
+    text = (
+        "📌 Send task in this format:\n\n"
+        "Title | Type | Link | Reward\n\n"
+        "Example:\n"
+        "Join Jaguar Channel|channel|https://t.me/Jaguar|500"
+    )
+
+    await message.answer(text)
